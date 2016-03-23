963c80a1760b0af7f79d3e99851fc9ffff3847ef
Plailect <Plailect@gmail.com>
2016-03-22 22:21:24 -0400

    renamed things

--- source/Updates/UpdateInfo.h
+++ source/Updates/UpdateInfo.h
@@ -2,8 +2,8 @@
 // Created by cpasjuste on 11/01/16.
 //
 
-#ifndef SAFESYSUPDATER_UPDATEINFO_H
-#define SAFESYSUPDATER_UPDATEINFO_H
+#ifndef PLAISYSUPDATER_UPDATEINFO_H
+#define PLAISYSUPDATER_UPDATEINFO_H
 
 #include <string>
 #include <vector>
@@ -19,4 +19,4 @@ public:
 };
 
 
-#endif //SAFESYSUPDATER_UPDATEINFO_H
+#endif //PLAISYSUPDATER_UPDATEINFO_H
